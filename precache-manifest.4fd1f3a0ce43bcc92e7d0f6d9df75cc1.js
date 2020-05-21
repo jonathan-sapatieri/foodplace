24b567404c94b231996a4dbe02408679dd056966
Jonathan Sapatieri <contato@jonathansapatieri.com>
2020-05-21 17:10:13 -0300

    Updates

---
+++ precache-manifest.4fd1f3a0ce43bcc92e7d0f6d9df75cc1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d7fc1e3e91ac6632533d849cad2910",
+    "url": "/foodplace/index.html"
+  },
+  {
+    "revision": "577f319a2f5e23fef260",
+    "url": "/foodplace/static/css/main.5fbfe0e0.chunk.css"
+  },
+  {
+    "revision": "e4d7624f76af6151d1ec",
+    "url": "/foodplace/static/js/2.698eb665.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodplace/static/js/2.698eb665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577f319a2f5e23fef260",
+    "url": "/foodplace/static/js/main.de701966.chunk.js"
+  },
+  {
+    "revision": "59bcce49026c3071d916",
+    "url": "/foodplace/static/js/runtime-main.fa687d8a.js"
+  },
+  {
+    "revision": "7935f3ef7bf499069e491535ad481e60",
+    "url": "/foodplace/static/media/bkg_business.7935f3ef.jpg"
+  },
+  {
+    "revision": "a0aa6d98b8e9ae9e1d74844fa3799c5e",
+    "url": "/foodplace/static/media/bkg_search_desktop.a0aa6d98.jpg"
+  },
+  {
+    "revision": "cf9dbfa24e3bc2986c491141e7c34978",
+    "url": "/foodplace/static/media/bkg_search_mobile.cf9dbfa2.jpg"
+  }
+]);
